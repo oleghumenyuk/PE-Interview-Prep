228a2e4afaaa0ceca2f05cc620e8b474cad15b24
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-10 13:58:00 -0800

    working solution

--- ReadLastK.java
+++ ReadLastK.java
@@ -1,3 +1,10 @@
+import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
 public class ReadLastK {
 
     /*
@@ -18,6 +25,39 @@ public class ReadLastK {
      */
 
     public static void main (String[] args) {
+        /*
+        1.) open file, stream input via Buffered Reader
+        2.) iterate through file
+            if circular queue size is greater than K, 
+                -> remove the 0th element (queue.poll() means remove head)
+                -> add to the end of the list (queue.offer() means add to tail)
+        */
 
+        int k = 150;
+        Queue<String> circularQueue = new LinkedList<>();
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String line;
+            while((line = reader.readLine()) != null) {
+
+                if(circularQueue.size() < k) {
+                    circularQueue.offer(line);
+                } else {
+                    //remove head
+                    circularQueue.poll();
+                    //add to tail
+                    circularQueue.offer(line);
+                }
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+       for(String line: circularQueue) {
+            System.out.println(line);
+       }
+        
     }
 }
