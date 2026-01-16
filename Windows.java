812e1c2f563ee6323a6d726172a61d901627731e
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-16 13:25:51 -0800

    setup next problem

---
+++ Windows.java
@@ -0,0 +1,30 @@
+
+public class Windows{
+
+    /*
+     * Problem: Merge Time Intervals (Maintenance Window)
+     *
+     * The Task:
+     *   You are given a text file of server maintenance windows (start time, end time).
+     *   Merge all overlapping windows to determine the total downtime.
+     *
+     * Input Example:
+     *   09:00,10:30
+     *   10:00,11:00
+     *
+     * Output Example:
+     *   09:00,11:00
+     *
+     * Why it's crucial:
+     *   Tests your ability to convert String timestamps into comparable objects
+     *   (or integers) and apply logical sorting.
+     *
+     * Java Focus:
+     *   - List.sort() for sorting intervals
+     *   - Custom Comparator for comparing time intervals
+     *   - class Interval for representing time windows
+     */
+    public static void main(String[] args) {
+
+    }
+}
