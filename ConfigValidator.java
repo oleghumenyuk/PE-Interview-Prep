bdbfa943f3e18abb199a60f86483657250661b35
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-17 15:38:44 -0800

    config validation

--- ConfigValidator.java
+++ ConfigValidator.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConfigValidator {
 
     /*
@@ -26,12 +30,57 @@ public class ConfigValidator {
      */
     public static void main(String[] args) {
         //open file via BufferedReader
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        //process line by line
+            //process line by line
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                processLine(line);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+            
+       
 
         //inmplement helper method to check for validity
 
         //do validation analysis
 
+    }
+
+    static void processLine(String line) {
+
+        //inmplement helper method to check for validity
+
+        //rule 1: ignore empty lline
+        if(line.isEmpty() || line.isBlank()) {
+            System.out.println("empty line");
+            return;
+        }
+
+        //rule2: ignore anything that starts with #
+        if(line.startsWith("#")) {
+            System.out.println("line is invalid");
+            return;
+        }
+
+        //rule3: , & : are valid regexes
+        String[] machineInfo = line.split("[,:]");
+
+        //rule 4: print only if invalid
+        //machineName: CPU cores: GB ram
+        // Valid: A machine is valid if it has at least 4 CPU cores AND at least 8 GB of RAM
+
+        int cpuCores = Integer.parseInt(machineInfo[1].trim());
+        int ram = Integer.parseInt(machineInfo[2].trim());
+
+        if(cpuCores < 4 || ram < 8) {
+            System.out.println("invalid config machine name: " + machineInfo[0]);
+            System.out.println(machineInfo[0] + " " + machineInfo[1] + " " + machineInfo[2]);
+        }
+
+        
     }
 }
