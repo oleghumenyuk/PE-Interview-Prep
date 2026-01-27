90b7a007585c7698cbab3134a9fea2c260faa465
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-27 15:57:00 -0800

    setup next problem

---
+++ ReplaceSensitiveChars.java
@@ -0,0 +1,12 @@
+public class ReplaceSensitiveChars {
+
+    /*
+     * Given a log file, replace all sensitive patterns
+     * (like Credit Card numbers or API keys) with "XXXX".
+     *
+     * Java Focus: Regular Expressions (Pattern and Matcher).
+     */
+    public static void main(String[] args) {
+
+    }
+}
