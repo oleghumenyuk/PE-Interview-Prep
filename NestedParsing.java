bad50e2b4fc80b0e17ec3ce6206700706230cde4
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-28 14:35:43 -0800

    setup nested parsing problem

---
+++ NestedParsing.java
@@ -0,0 +1,14 @@
+public class NestedParsing {
+
+    /*
+    
+    4. **Coding (Harder):** Flatten a nested Dictionary/Map.
+    - *Input:* `{"a": 1, "b": {"c": 2, "d": {"e": 3}}}`
+    - *Output:* `{"a": 1, "b.c": 2, "b.d.e": 3}`
+    - *Java Focus:* Recursion, passing state (current key prefix) down the stack.
+    */
+
+    public static void main(String[] args) {
+
+    }
+}
