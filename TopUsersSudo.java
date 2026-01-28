7ab9ca01e4113cf321fcd47de2837c3a88515b88
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-28 15:15:09 -0800

    working solution

--- TopUsersSudo.java
+++ TopUsersSudo.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class TopUsersSudo {
 
     /*
@@ -10,5 +18,54 @@ public class TopUsersSudo {
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> sudoCounts = new HashMap<>();
+
+        try {
+
+            //open file
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while((line = bufferedReader.readLine()) != null) {
+            String[] logData = line.split("\\s+");
+            //logData[3] must be sudo
+            if(logData[3].equals("sudo")) {
+                String user = logData[2];
+
+                if(sudoCounts.containsKey(user)) {
+                    int previousTimes = sudoCounts.get(user);
+                    sudoCounts.put(user, previousTimes + 1);
+
+                } else {
+                    sudoCounts.put(user, 1);
+                }
+            }
+        }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        //after we process logs, we store top 5 users in min heap
+        //if min heap is over 5, we remove the lowest user
+
+
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> a.getValue() - b.getValue()
+        );
+
+        for(Map.Entry<String, Integer> entry : sudoCounts.entrySet()) {
+            pq.add(entry);
+
+            if(pq.size() > 5) {
+                pq.poll();
+            }
+            
+        }
+
+        System.out.println(pq.toString());
+
     }
+
+
 }
