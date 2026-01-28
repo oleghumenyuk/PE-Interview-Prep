b790747c5baa428499b58b04954d93d6d51088c2
Oleg Humenyuk <oleghumenyuk@Olegs-MacBook-Air.local>
2026-01-28 14:34:15 -0800

    setup top Users sudo problem

---
+++ TopUsersSudo.java
@@ -0,0 +1,14 @@
+public class TopUsersSudo {
+
+    /*
+     * 2. Parse a raw text file containing executed command history:
+     *    `[timestamp] user: command`.
+     *    Find the top 5 users who ran `sudo`.
+     *
+     * Java Focus: String parsing logic combined with HashMap.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
